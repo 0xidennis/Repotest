746e15604d1a20a853582a3c99267b74b636a5b1
0xidennis <156088691+0xidennis@users.noreply.github.com>
2025-02-11 16:05:45 -0800

    first commit

--- src/card/Upcoming.jsx
+++ src/card/Upcoming.jsx
@@ -1,5 +1,7 @@
 import './Upcoming.css'
 import { FiClock } from 'react-icons/fi';
+import img from '../assets/images/img.png'
+import { FaFileImage } from 'react-icons/fa';
 const Upcoming = () => {
   return (
     <div className="exam-card">
@@ -10,9 +12,9 @@ const Upcoming = () => {
       <h2 className="exam-title">Exam - Unit 5</h2>
       <p className="exam-subtitle">Spanish for beginners</p>
       <div className="participants">
-        <img src="/path-to-avatar1.jpg" alt="Participant 1" className="participant-avatar" />
-        <img src="/path-to-avatar2.jpg" alt="Participant 2" className="participant-avatar" />
-        <img src="/path-to-avatar3.jpg" alt="Participant 3" className="participant-avatar" />
+        <img src={img} alt="Participant 1" className="participant-avatar" />
+        <img src={img} alt="Participant 2" className="participant-avatar" />
+        <img src={img} alt="Participant 3" className="participant-avatar" />
         <div className="more-participants">+99</div>
       </div>
     </div>
